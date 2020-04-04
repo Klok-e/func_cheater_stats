a0a65fa8ad8cacd2fb8d7d97ab38c5b5d2b0695c
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2020-04-04 11:54:13 +0300

    add logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        }).level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("logs.log")?)
+        .apply()?;
 }
