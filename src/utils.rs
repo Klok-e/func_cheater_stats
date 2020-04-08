41afa50edde6f69e15a8a0717524092aca7e0a82
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2020-04-08 20:32:59 +0300

    add utils

---
+++ src/utils.rs
@@ -0,0 +1,18 @@
+use std::convert::identity;
+
+pub fn chunk_with_size(s: &str) -> Vec<String> {
+    const MAX_CHUNK_SIZE: usize = 1024;
+
+    let mut chunks = vec!["".to_owned()];
+    for line in s.lines() {
+        if chunks.last().unwrap().len() + line.len() < MAX_CHUNK_SIZE {
+            chunks
+                .last_mut()
+                .unwrap()
+                .push_str(format!("{}\n", line).as_str());
+        } else {
+            chunks.push(format!("{}\n", line))
+        }
+    }
+    chunks
+}
